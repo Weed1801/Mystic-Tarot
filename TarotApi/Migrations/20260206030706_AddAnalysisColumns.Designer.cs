7d12e0bb91325792f33bd5dadaa05374ce211e27
Weed1801 <haianh18012004@gmail.com>
2026-02-06 10:17:48 +0700

    Backup: Save current state with sound fixes and new migrations

--- TarotApi/Migrations/20260206030706_AddAnalysisColumns.Designer.cs
+++ TarotApi/Migrations/20260206030706_AddAnalysisColumns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TarotApi.Data;
@@ -11,9 +12,11 @@ using TarotApi.Data;
 namespace TarotApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260206030706_AddAnalysisColumns")]
+    partial class AddAnalysisColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TarotApi/Migrations/20260206030706_AddAnalysisColumns.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TarotApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnalysisColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FinalAdvice",
+                table: "ReadingSessions",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FutureAnalysis",
+                table: "ReadingSessions",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PastAnalysis",
+                table: "ReadingSessions",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PresentAnalysis",
+                table: "ReadingSessions",
+                type: "character varying(4000)",
+                maxLength: 4000,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinalAdvice",
+                table: "ReadingSessions");
+
+            migrationBuilder.DropColumn(
+                name: "FutureAnalysis",
+                table: "ReadingSessions");
+
+            migrationBuilder.DropColumn(
+                name: "PastAnalysis",
+                table: "ReadingSessions");
+
+            migrationBuilder.DropColumn(
+                name: "PresentAnalysis",
+                table: "ReadingSessions");
+        }
+    }
+}
